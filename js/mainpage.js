b84989f97c0411744f3eacedb110a16719dc65d5
Tahir <mitchelt6@hawkmail.newpaltz.edu>
2020-12-13 19:38:44 -0500

    updated logo size and removed countdown

--- js/mainpage.js
+++ js/mainpage.js
@@ -5,13 +5,13 @@
 
 
 
-$(document).ready(function () {
+$(document).ready(function() {
 
     document.cookie = 'cross-site-cookie =bar; SameSite=Strict; Secure';
 
     var isFirefox = typeof InstallTrigger !== 'undefined';
 
-    if(isFirefox){
+    if (isFirefox) {
         $(".nav-item").css({
             "-webkit-text-stroke": "20px",
             "-webkit-text-stroke-width": "20px",
@@ -30,84 +30,83 @@ $(document).ready(function () {
             "font-weight": "unset",
             "text-shadow": "unset"
         })
-    }
-    else{
+    } else {
         $(".nav-item").css({
-    "-webkit-text-stroke": "thick",
-      "letter-spacing": "1.5px"
+            "-webkit-text-stroke": "thick",
+            "letter-spacing": "1.5px"
         });
 
         $(".countdown-header").css({
-    "font-family": "PfefferMediaeval",
-    "-webkit-text-stroke": "thick",
-    "letter-spacing": "3.2px",
-    "font-size": "25px"
+            "font-family": "PfefferMediaeval",
+            "-webkit-text-stroke": "thick",
+            "letter-spacing": "3.2px",
+            "font-size": "25px"
         })
     }
-    
-    
-    
-    $("#dconinfo-section, #home-section, #gallery-section, #elections-section, #register-section, #awards-section, #involvement-section, #location-section, #contact-section, #elections-section").hide();
 
-    $(".gallery-link").click(function () {
+
+
+    $("#dconinfo-section, #countdown-section, #gallery-section, #elections-section, #register-section, #awards-section, #involvement-section, #location-section, #contact-section, #elections-section").hide();
+
+    $(".gallery-link").click(function() {
         $("#countdown-section, #dconinfo-section, #register-section, #awards-section, #involvement-section, #location-section, #contact-section, #elections-section").hide();
         $('#gallery-section').show();
     });
 
-    $('.home-link').click(function () {
+    $('.home-link').click(function() {
         $("#dconinfo-section, #gallery-section, #register-section, #awards-section, #involvement-section, #location-section, #contact-section, #elections-section").hide();
         $("#countdown-section").show();
     })
 
-    $(".register-link").click(function () {
+    $(".register-link").click(function() {
         $("#gallery-section, #dconinfo-section, #countdown-section, #awards-section, #involvement-section, #location-section, #contact-section, #elections-section").hide();
         $("#register-section").show();
     });
 
-    $(".awards-link").click(function () {
+    $(".awards-link").click(function() {
         $("#gallery-section, #dconinfo-section, #countdown-section, #register-section, #involvement-section, #location-section, #contact-section, #elections-section").hide();
         $("#awards-section").show();
     });
 
-    $(".involvement-link").click(function () {
+    $(".involvement-link").click(function() {
         $("#gallery-section, #countdown-section, #dconinfo-section, #register-section, #awards-section, #location-section, #contact-section, #elections-section").hide();
         $("#involvement-section").show();
     });
-    $(".location-link").click(function () {
+    $(".location-link").click(function() {
         $("#gallery-section, #countdown-section, #dconinfo-section, #register-section, #awards-section, #involvement-section, #contact-section, #elections-section").hide();
         $("#location-section").show();
     });
-    $(".contact-link").click(function () {
+    $(".contact-link").click(function() {
         $("#gallery-section, #countdown-section, #dconinfo-section, #register-section, #awards-section, #involvement-section, #location-section, #elections-section").hide();
         $("#contact-section").show();
     });
-    $(".elections-link").click(function(){
+    $(".elections-link").click(function() {
         $("#gallery-section, #countdown-section, #dconinfo-section, #register-section, #awards-section, #involvement-section, #location-section, #contact-section").hide();
         $('#elections-section').show();
     })
-    
-    $('.down-arrow').click(function(){
+
+    $('.down-arrow').click(function() {
         $('#countdown-section').hide();
         $('#dconinfo-section').show();
-    })
-//Sourced from https://github.com/epiksel/countdown
-    var now = new Date();
-	var day = 6;//now.getDate();
-	var month = 3; //now.getMonth() + 1;
-	var year = 2030;//now.getFullYear() + 1;
+    });
+    // //Sourced from https://github.com/epiksel/countdown
+    //     var now = new Date();
+    // 	var day = 6;//now.getDate();
+    // 	var month = 3; //now.getMonth() + 1;
+    // 	var year = 2030;//now.getFullYear() + 1;
 
-		var nextyear = month + '/' + day + '/' + year + ' 16:00:00';
+    // 		var nextyear = month + '/' + day + '/' + year + ' 16:00:00';
 
-		$('#example').countdown({
-			date: nextyear, // TODO Date format: 07/27/2017 17:00:00
-			offset: -5, // TODO Your Timezone Offset
-			day: 'Day',
-			days: 'Days'
-		}, function () {
-            $('.countdown-header-static').show();
-            $('#location-section').show();
-            $('#countdown-section').hide();
-        });
-        
-       
+    // 		$('#example').countdown({
+    // 			date: nextyear, // TODO Date format: 07/27/2017 17:00:00
+    // 			offset: -5, // TODO Your Timezone Offset
+    // 			day: 'Day',
+    // 			days: 'Days'
+    // 		}, function () {
+    //             $('.countdown-header-static').show();
+    //             $('#location-section').show();
+    //             $('#countdown-section').hide();
+    //         });
+
+
 });
